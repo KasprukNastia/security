b1b8c5f2a526f3bae7eb0f0a5f5026f7302eca6a
Настя Каспрук <anastasiyakaspruk17@gmail.com>
2020-10-14 19:42:50 +0300

    added class

---
+++ Lab1/Lab1/Task1/SingleByteXor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1.Task1
+{
+    public class SingleByteXor
+    {
+        public string Encrypt(string message, byte key)
+        {
+            return string.Empty;
+        }
+    }
+}
