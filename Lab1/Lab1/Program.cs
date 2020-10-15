8d1c7fadf36b42b21b2db0b8463327f79fe63cd3
Anastasiya Kaspruk <anastasiyakaspruk17@gmail.com>
2020-10-15 19:53:58 +0300

    decrypted second task

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lab1.Task1;
+using System;
 
 namespace Lab1
 {
@@ -6,7 +7,15 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string encryptedMessage = "Yx`7cen7v7ergrvc~yp:|rn7OXE7t~g.re97R9p97~c7d.xb{s7cv|r7v7dce~yp75.r{{x7`xe{s57vys;7p~ary7c.r7|rn7~d75|rn5;7oxe7c.r7q~edc7{rccre75.57`~c.75|5;7c.ry7oxe75r57`~c.75r5;7c.ry75{57`~c.75n5;7vys7c.ry7oxe7yroc7t.ve75{57`~c.75|57vpv~y;7c.ry75x57`~c.75r57vys7dx7xy97Nxb7zvn7bdr7vy7~ysro7xq7tx~yt~srytr;7_vzz~yp7s~dcvytr;7\vd~d|~7rovz~yvc~xy;7dcvc~dc~tv{7crdcd7xe7`.vcrare7zrc.xs7nxb7qrr{7`xb{s7d.x`7c.r7urdc7erdb{c9";
+
+            byte key = new SingleByteXorAttacker().GetSingleByteXorKey(encryptedMessage);
+
+            string decryptedMessage = new SingleByteXor().Decrypt(encryptedMessage, key);
+
+            Console.WriteLine(decryptedMessage);
+            Console.WriteLine();
+            Console.WriteLine(decryptedMessage.Replace('9', 'h'));
         }
     }
 }
